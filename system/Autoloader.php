2d8eaec01081e5fb99f01dd7ff0e8bb2dd5ff3e8
sharf <373455742@qq.com>
2016-07-07 19:53:50 +0800

    234

--- system/Autoloader.php
+++ system/Autoloader.php
@@ -16,9 +16,11 @@ class Autoloader
         }
         //echo $class_file.'<br><br><br>';
         if (is_file($class_file)) {
+            echo $class_file;
             require_once($class_file);
+            echo 1;
+            echo $name;
             if (class_exists($name, false)) {
-                echo $name;
                 return true;
             }
         }
