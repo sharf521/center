59afc3420f210f3e24931139708da03e5991ee96
sharf <373455742@qq.com>
2016-08-04 15:55:42 +0800

    add wap

--- app/Controller/Member/IndexController.php
+++ app/Controller/Member/IndexController.php
@@ -13,6 +13,7 @@ class IndexController extends MemberController
 
     public function index()
     {
+        echo $this->user_id;
         $data['account'] = DB::table('account')->where('user_id')->bindValues($this->user_id)->row();
         $data['title_herder']='帐户中心';
         $this->view('manage', $data);
