e04a773f994a70bd91f054f8922ae3aee89b7288
sharf <373455742@qq.com>
2016-08-04 14:36:54 +0800

    add wap

--- app/themes/default_wap/header.php
+++ app/themes/default_wap/header.php
@@ -9,7 +9,7 @@
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta http-equiv="Expires" content="-1">
     <meta http-equiv="pragram" content="no-cache">
-    <meta name="viewport" content="width=640, user-scalable=no, target-densitydpi=device-dpi,initial-scale=1, maximum-scale=1">
+    <meta name="viewport" content="width=640, user-scalable=no, target-densitydpi=device-dpi">
     <title>新用户注册</title>
     <link href="/themes/default_wap/css/web_style.css" type="text/css" rel="stylesheet"/>
     <link href="/themes/default_wap/css/web_login.css" type="text/css" rel="stylesheet"/>
