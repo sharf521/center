576355ed70b845d4025762ba2b8e2c9dbb55a20c
sharf <373455742@qq.com>
2016-08-26 17:37:31 +0800

    add    wap tpl

--- app/themes/member_wap/header.php
+++ app/themes/member_wap/header.php
@@ -11,8 +11,8 @@
     <meta http-equiv="pragram" content="no-cache">
     <meta name="viewport" content="width=640, user-scalable=no, target-densitydpi=device-dpi">
     <title><?=$title_herder?></title>
-    <link href="/themes/member_wap/css/common.css" type="text/css" rel="stylesheet"/>
-    <link href="/themes/member_wap/css/account.css" type="text/css" rel="stylesheet"/>
+    <link href="/themes/member_wap/css/common.css?<?=rand(10000,99999)?>" type="text/css" rel="stylesheet"/>
+    <link href="/themes/member_wap/css/account.css?<?=rand(10000,99999)?>" type="text/css" rel="stylesheet"/>
     <script type="text/javascript" src="/themes/member_wap/js/jquery-1.11.1.min.js"></script>
     <script type="text/javascript" src="/themes/member_wap/member.js"></script>
 </head>
