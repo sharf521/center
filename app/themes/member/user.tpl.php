266accfdbf14b1948bd902a2ba6364856fcc43bd
sharf <373455742@qq.com>
2016-10-21 13:54:23 +0800

    layui

--- app/themes/member/user.tpl.php
+++ app/themes/member/user.tpl.php
@@ -16,7 +16,7 @@
                         <tr><td>头像：</td><td>
                                 <span id="upload_span_headimgurl"><img src="<?=$user->headimgurl?>" height="50"></span>
                                 <input type="hidden" name="headimgurl" value="<?=$user->headimgurl?>" id="headimgurl">
-                                <input type="file" name="files" class="layui-upload-file" upload_id="headimgurl" upload_type="headimgurl">
+                                <input type="file" name="file" class="layui-upload-file" upload_id="headimgurl" upload_type="headimgurl">
                             </td></tr>
                         <tr><td>联系电话：</td><td><input type="text" name="tel" value="<?=$user->tel?>"/></td></tr>
                         <tr><td>联系QQ： </td><td><input type="text" name="qq" class="form-control" value="<?=$user->qq?>" onKeyUp="value=value.replace(/[^0-9.]/g,'')"/></td></tr>
