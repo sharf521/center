29d3ccc6f83a96f33bd649d4f75940cde73e52da
sharf <373455742@qq.com>
2016-12-03 14:16:30 +0800

    app

--- app/themes/member_wap/manage.tpl.php
+++ app/themes/member_wap/manage.tpl.php
@@ -58,7 +58,7 @@
         }
     </script>
     <ul>
-        <li><a href="javascript:mAlert();"><!--<?= url('goApp/5/wap') ?>-->
+        <li><a href="<?= url('goApp/5/wap') ?>">
                 <b><img src="/themes/member_wap/images/icon_shop.png"/></b>
                 <i><img src="/themes/member_wap/images/icon_shop_cor.png"/></i>
                 <p>汽车</p>
