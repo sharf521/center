072437bdab93d36cd33be7bb8df928f287958559
sharf <373455742@qq.com>
2017-04-10 12:32:16 +0800

    保存wechat_openid

--- index.php
+++ index.php
@@ -10,7 +10,9 @@ header('Cache-Control: private',false); // required for certain browsers
 header('Cache-Control: no-cache, no-store, max-age=0, must-revalidate');
 header('Expires: '.gmdate('D, d M Y H:i:s') . ' GMT');
 header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
-//获取域名
+/*
+ * //session 里user_id 和其它应用里user_id 不一致 ，不能共享session
+ * //获取域名
 $domain=strtolower($_SERVER['HTTP_HOST']);
 if(strpos($domain,':')!==false){
     //去除端口
@@ -24,7 +26,7 @@ if($domain_arr[count($domain_arr)-2]=='com'){
     $domain=$domain_arr[count($domain_arr)-2].'.'.$domain_arr[count($domain_arr)-1];
 }
 //session_start();之前设置  php.ini 里 session.auto_start=0
-ini_set('session.cookie_domain', $domain);//域名不需要端口
+ini_set('session.cookie_domain', $domain);//域名不需要端口*/
 session_cache_limiter('private,must-revalidate');
 session_start();
 date_default_timezone_set('Asia/Shanghai');//时区配置
