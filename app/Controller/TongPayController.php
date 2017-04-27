8a7db096f48ab165eca170268eabdc69efe248fd
sharf <373455742@qq.com>
2017-04-27 15:27:40 +0800

    分期

--- app/Controller/TongPayController.php
+++ app/Controller/TongPayController.php
@@ -33,7 +33,7 @@ class TongPayController extends Controller
             'pdno'=>'0200',
             'v'=>'1.0',
             'sign_v'=>'1',
-            'amount'=>'2000.00',
+            'amount'=>'10000.00',
             'channel'=>'0',//支付渠道：0：pc   1：wap
             'comment'=>'',
             'description'=>'',
@@ -44,8 +44,15 @@ class TongPayController extends Controller
             'timestamp'=>date('YmdHis'),
             'trade_date'=>date('Ymd'),
             'trade_time'=>date('His'),
-            'unalter'=>'nper'
+            'unalter'=>'nper',
+            'cetitype'=>'01'
         );
+        $para['creditName']='李超';
+        $para['idno']=$this->des_encrypt('230715199203234023');
+        $para['phoneNo']=$this->des_encrypt('13937127756');
+        $para['creditNo']=$this->des_encrypt('6259983790374402');
+        $para['validty_period']=$this->des_encrypt('0418');
+        $para['cvv']=$this->des_encrypt('838');
 
         $data = array(
             'trade_no' => $para['order_id'],
@@ -69,6 +76,136 @@ class TongPayController extends Controller
         $sHtml = $sHtml . "<input type='submit'></form>";
         //$sHtml = $sHtml . "<script>document.forms['fupaysubmit'].submit();</script>";
         echo $sHtml;
+    }
+
+
+    /*        $arr = array(
+            'msg' => '消费成功',
+            'nper' => '12',
+            'orderId' => '149320225680298',
+            'result' => '1',
+            'totalAmt' => '2127.80',
+            'sign' => '0720794c53a495dbb3438cb381c96534fcb12ed0df3c2fef719917335d85c647f2c81ae7ebebc63bd7ed48994a7458aaee1c8c2273f6ddeeceb02b13a0b189b79615f4b837a313948971e4960241491e0a81f70a1a7d1182dd1c4d5c5aafaf40b95b9dac2588bbfc093861f289c02a85d4a24de0708874c1ef60f7dba998dd2d'
+        );*/
+    public function result(Request $request, Account $account)
+    {
+        $this->log($_POST);
+        $checkSign = $this->checkSign($_POST);
+        if(!$checkSign){
+            $this->log('checkSign error');
+            exit;
+        }
+        $msg=$request->post('msg');
+        $nper=$request->post('nper');//12
+        $totalAmt=$request->post('totalAmt');//2127.80
+        $UsrSn=$request->post('orderId');
+        $result=$request->post('result');
+        if($result!=1){
+            echo 'success';
+            exit;
+        }
+        //判断缓存中是否有 创建交易cache缓存文件
+        $path = ROOT . "/public/data/pay_cache/" . date("Y-m") . "/";
+        if (!file_exists($path)) {
+            mkdir($path,0777,true);
+        }
+        $file = $path . $UsrSn;
+        $fp = fopen($file, 'w+');
+        chmod($file, 0777);
+        if (flock($fp, LOCK_EX | LOCK_NB)) //设定模式独占锁定和不堵塞锁定
+        {
+            ////////////start处理////////////////////////////
+            $row = DB::table('account_recharge')->where("trade_no='{$UsrSn}'")->row();
+            if ($row) {
+                if ($row['status'] == 0) {
+                    DB::table('account_recharge')->where("trade_no='{$UsrSn}'")->update(array('status' => 1));
+                    $log = array();
+                    $log['user_id'] = $row['user_id'];
+                    $log['type'] = 1;
+                    $log['funds_available'] = $row['money'] - $row['fee'];
+                    $log['remark'] = "在线分期：" . $row['id'];
+                    $account->addLog($log);
+                }
+            }
+            echo 'success';
+            ///////////结束处理/////////////////////////////
+            flock($fp, LOCK_UN);
+        }
+        fclose($fp);
+    }
+
+    public function getOrder()
+    {
+        $url='http://gateway.ulinkpay.com:8002/asaop/rest/api/';
+        $para=array(
+            'app_key'=>'testhn',
+            'method'=>'allinpay.order.orderinstall.query',
+            'format'=>'json',
+            'mer_id'=>'999290053990002',
+            'pdno'=>'0200',
+            'v'=>'1.0',
+            'sign_v'=>'1',
+            'order_id'=>'TL149326676837772',
+            'timestamp'=>date('YmdHis')
+        );
+        $para['sign']=$this->sign($para);
+        print_r($para);
+        $sHtml = "<form id='fupaysubmit' name='fupaysubmit' action='{$url}' method='post' style='display:'>";
+        while (list ($key, $val) = each($para)) {
+            $sHtml .= "<input type='hidden' name='" . $key . "' value='" . $val . "'/>";
+        }
+        //submit按钮控件请不要含有name属性
+        $sHtml = $sHtml . "<input type='submit'></form>";
+        //$sHtml = $sHtml . "<script>document.forms['fupaysubmit'].submit();</script>";
+        echo $sHtml;
+        exit;
+        $html=$this->curl_url('http://gateway.ulinkpay.com:8002/asaop/rest/api/',$para);
+        echo $html;
+
+    }
+
+    private function curl_url($url, $data = array())
+    {
+        echo $url;
+        $ssl = substr($url, 0, 8) == "https://" ? TRUE : FALSE;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        if ($data) {
+            if (is_array($data)) {
+                curl_setopt($ch, CURLOPT_POST, 1);
+            } else {
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                        'Content-Type: application/json',
+                        'Content-Length: ' . strlen($data))
+                );
+            }
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        }
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        if ($ssl) {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        }
+        $data = curl_exec($ch);
+        var_dump($data);
+        curl_close($ch);
+        return $data;
+    }
+
+    //关键数据DES加密
+    private function des_encrypt ($encrypt,$key='a32df3e4')
+    {
+        // 根據 PKCS#7 RFC 5652 Cryptographic Message Syntax (CMS) 修正 Message 加入 Padding
+        $block = mcrypt_get_block_size(MCRYPT_DES, MCRYPT_MODE_ECB);
+        $pad = $block - (strlen($encrypt) % $block);
+        $encrypt .= str_repeat(chr($pad), $pad);
+
+        // 不需要設定 IV 進行加密
+        $passcrypt = mcrypt_encrypt(MCRYPT_DES, $key, $encrypt, MCRYPT_MODE_ECB);
+        return base64_encode($passcrypt);
     }
 
     private function sign($data)
@@ -112,61 +249,6 @@ class TongPayController extends Controller
         }
         fwrite($myfile, '【'.date('Y-m-d H:i:s').'】'.$data."\r\n");
         fclose($myfile);
-    }
-
-    public function result(Request $request, Account $account)
-    {
-        $this->log($_POST);
-/*        $arr = array(
-            'msg' => '消费成功',
-            'nper' => '12',
-            'orderId' => '149320225680298',
-            'result' => '1',
-            'totalAmt' => '2127.80',
-            'sign' => '0720794c53a495dbb3438cb381c96534fcb12ed0df3c2fef719917335d85c647f2c81ae7ebebc63bd7ed48994a7458aaee1c8c2273f6ddeeceb02b13a0b189b79615f4b837a313948971e4960241491e0a81f70a1a7d1182dd1c4d5c5aafaf40b95b9dac2588bbfc093861f289c02a85d4a24de0708874c1ef60f7dba998dd2d'
-        );*/
-        $checkSign = $this->checkSign($_POST);
-        if(!$checkSign){
-            $this->log('checkSign error');
-            exit;
-        }
-        $msg=$request->post('msg');
-        $nper=$request->post('nper');//12
-        $totalAmt=$request->post('totalAmt');//2127.80
-        $UsrSn=$request->post('orderId');  
-        $result=$request->post('result');
-        if($result!=1){
-            echo 'success';
-            exit;
-        }
-        //判断缓存中是否有 创建交易cache缓存文件
-        $path = ROOT . "/public/data/pay_cache/" . date("Y-m") . "/";
-        if (!file_exists($path)) {
-            mkdir($path,0777,true);
-        }
-        $file = $path . $UsrSn;
-        $fp = fopen($file, 'w+');
-        chmod($file, 0777);
-        if (flock($fp, LOCK_EX | LOCK_NB)) //设定模式独占锁定和不堵塞锁定
-        {
-            ////////////start处理////////////////////////////
-            $row = DB::table('account_recharge')->where("trade_no='{$UsrSn}'")->row();
-            if ($row) {
-                if ($row['status'] == 0) {
-                    DB::table('account_recharge')->where("trade_no='{$UsrSn}'")->update(array('status' => 1));
-                    $log = array();
-                    $log['user_id'] = $row['user_id'];
-                    $log['type'] = 1;
-                    $log['funds_available'] = $row['money'] - $row['fee'];
-                    $log['remark'] = "在线分期：" . $row['id'];
-                    $account->addLog($log);
-                }
-            }
-            echo 'success';
-            ///////////结束处理/////////////////////////////
-            flock($fp, LOCK_UN);
-        }
-        fclose($fp);
     }
 
     //参数排序
