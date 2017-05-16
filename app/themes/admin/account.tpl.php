c300c6f16b15c9af70494cfa8cbfa7b29e257eb2
sharf <373455742@qq.com>
2017-05-16 17:11:02 +0800

    table

--- app/themes/admin/account.tpl.php
+++ app/themes/admin/account.tpl.php
@@ -23,59 +23,57 @@
             <input  type="submit" value="查询" />
         </form>
     </div>
-    <div class="main_content">
-        <? if(!empty($result['total'])){?>
-            <table class="table">
-                <tr>
-                    <th>ID</th>
-                    <th>app</th>
-                    <th>支付单号</th>
-                    <th>商户订单号</th>
-                    <th>用户/ID</th>
-                    <th>时间</th>
-                    <th>类型</th>
-                    <th>label</th>
-                    <th>变动</th>
-                    <th>备注</th>
-                    <th>可用资金</th>
-                    <th>冻结资金</th>
-                    <th>可用积分</th>
-                    <th>冻结积分</th>
-                    <th>保证金</th>
-                    <th>可用周转金</th>
-                    <th>周转金额度</th>
+    <? if(!empty($result['total'])){?>
+        <table class="layui-table">
+            <tr>
+                <th>ID</th>
+                <th>app</th>
+                <th>支付单号</th>
+                <th>商户订单号</th>
+                <th>用户/ID</th>
+                <th>时间</th>
+                <th>类型</th>
+                <th>label</th>
+                <th>变动</th>
+                <th>备注</th>
+                <th>可用资金</th>
+                <th>冻结资金</th>
+                <th>可用积分</th>
+                <th>冻结积分</th>
+                <th>保证金</th>
+                <th>可用周转金</th>
+                <th>周转金额度</th>
 
-                </tr>
-                <? foreach($result['list'] as $row){
-                    ?>
-                    <tr>
-                        <td><?=$row->id?></td>
-                        <td><?=$row->App()->name?></td>
-                        <td><?=$row->pay_no?></td>
-                        <td><?=$row->app_order_no?></td>
-                        <td><?=$row->user()->username?>/<?=$row->user()->id?></td>
-                        <td><?=$row->created_at?></td>
-                        <td><?=$row->getLinkPageName('account_type',$row->type);?></td>
-                        <td class="fl"><?=$row->label?></td>
-                        <td class="fl"><?=$row->change?></td>
-                        <td class="fl"><?=nl2br($row->remark)?></td>
-                        <td class="fl">￥<?=(float)$row->funds_available_now?></td>
-                        <td class="fl">￥<?=(float)$row->funds_freeze_now?></td>
-                        <td class="fl">￥<?=(float)$row->integral_available_now?></td>
-                        <td class="fl">￥<?=(float)$row->integral_freeze_now?></td>
-                        <td class="fl">￥<?=(float)$row->security_deposit_now?></td>
-                        <td class="fl">￥<?=(float)$row->turnover_available_now?></td>
-                        <td class="fl">￥<?=(float)$row->turnover_credit_now?></td>
-                    </tr>
-                <? }
-                $sum=$result['sum'];
+            </tr>
+            <? foreach($result['list'] as $row){
                 ?>
-            </table>
-        <? }else{?>
-            <div class="alert-warning" role="alert">无记录！</div>
-        <? }?>
-        <?=$result['page'];?>
-    </div>
+                <tr>
+                    <td><?=$row->id?></td>
+                    <td><?=$row->App()->name?></td>
+                    <td><?=$row->pay_no?></td>
+                    <td><?=$row->app_order_no?></td>
+                    <td><?=$row->user()->username?>/<?=$row->user()->id?></td>
+                    <td><?=$row->created_at?></td>
+                    <td><?=$row->getLinkPageName('account_type',$row->type);?></td>
+                    <td class="fl"><?=$row->label?></td>
+                    <td class="fl"><?=$row->change?></td>
+                    <td class="fl"><?=nl2br($row->remark)?></td>
+                    <td class="fl">￥<?=(float)$row->funds_available_now?></td>
+                    <td class="fl">￥<?=(float)$row->funds_freeze_now?></td>
+                    <td class="fl">￥<?=(float)$row->integral_available_now?></td>
+                    <td class="fl">￥<?=(float)$row->integral_freeze_now?></td>
+                    <td class="fl">￥<?=(float)$row->security_deposit_now?></td>
+                    <td class="fl">￥<?=(float)$row->turnover_available_now?></td>
+                    <td class="fl">￥<?=(float)$row->turnover_credit_now?></td>
+                </tr>
+            <? }
+            $sum=$result['sum'];
+            ?>
+        </table>
+    <? }else{?>
+        <div class="alert-warning" role="alert">无记录！</div>
+    <? }?>
+    <?=$result['page'];?>
     <div>总计：
         可用资金：￥<?=(float)$sum->funds_available?><br>
             冻结资金：￥<?=(float)$sum->funds_freeze?><br>
