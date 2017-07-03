82353314d8234b6f74e3450f573c8971c8d13352
sharf <373455742@qq.com>
2017-07-03 17:24:46 +0800

    ..

--- app/Controller/LoginController.php
+++ app/Controller/LoginController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use App\Helper;
 use App\Model\User;
 use System\Lib\Request;
 
@@ -33,36 +34,7 @@ class LoginController extends Controller
             redirect()->back()->with('error',$error);
         }else{
             if($this->is_wap && $this->is_inWeChat){
-                session()->remove('wechat_openid');
-                $get_wechat_openid = $request->get('wechat_openid');
-                if(empty($get_wechat_openid)){
-                    $this_url='http://'.$_SERVER['HTTP_HOST'].urlencode($_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING']);
-                    $url = "http://wx02560f146a566747.wechat.yuantuwang.com/user/getWeChatOpenId/?url={$this_url}";
-                    redirect($url);
-                }else{
-                    $wechat_openid=$get_wechat_openid;
-                }
-                $user=$user->where('wechat_openid=?')->bindValues($wechat_openid)->first();
-                if($user->is_exist){
-                    $_data=array(
-                        'direct'=>1,
-                        'id'=>$user->id
-                    );
-                    $result=$user->login($_data);
-                    if ($result === true) {
-                        $url=$request->get('url');
-                        if(empty($url)){
-                            redirect('member/');
-                        }else{
-                            header("location:$url");exit;
-                        }
-                    } else {
-                        $error = $result;
-                    }
-                    redirect()->back()->with('error',$error);
-                }else{
-                    session()->set('wechat_openid',$wechat_openid);
-                }
+                Helper::wechatAutoLogin();
             }
             $regUrl="/register/";
             $getPwdUrl="/getPwd/";
