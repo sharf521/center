3e017ac2751e50621878d347f60f7c71fb558ffa
sharf <373455742@qq.com>
2017-08-09 14:27:42 +0800

    fbb test

--- app/Controller/Admin/FbbController.php
+++ app/Controller/Admin/FbbController.php
@@ -103,6 +103,7 @@ class FbbController extends AdminController
 
     function test1000UserReg()
     {
+        exit;
         //32767
         for ($i=1;$i<=32767;$i++){
             $data = array(
@@ -121,7 +122,7 @@ class FbbController extends AdminController
 
     function test1000FbbReg()
     {
-        $userList=(new User())->select('id')->where("id>=1011")->limit("30000,5000")->get();
+        $userList=(new User())->select('id')->where("id>=1500")->limit("0,5000")->get();
         //$userList=DB::table('user u')->select("u.id")->leftJoin('fbb b','u.id=b.user_id')->where()
         //SELECT * FROM `plf_user` u WHERE 1 and not EXISTS (SELECT 1 FROM plf_fbb b WHERE u.id = b.user_id)
         foreach ($userList as $user){
