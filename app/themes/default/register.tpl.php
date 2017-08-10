2c64ad337531f5edd3ae3822462fde859a049a37
sharf <373455742@qq.com>
2017-08-10 11:51:59 +0800

    test

--- app/themes/default/register.tpl.php
+++ app/themes/default/register.tpl.php
@@ -28,7 +28,8 @@
     jQuery.validator.addMethod("isPhone", function(value, element) {
         var length = value.length;
         var mobile = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1})|(17[0-9]{1}))+\d{8})$/;
-        return this.optional(element) || (length == 11 && mobile.test(value));
+        //return this.optional(element) || (length == 11 && mobile.test(value));
+        return true;
     }, "请填写正确的手机号码");//可以自定义默认提示信息
     $(document).ready(function(){
         $('#login_form').validate({
